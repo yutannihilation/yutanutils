ca2b132526e200d7bc32ef9d933575a7d5336005
Hiroaki Yutani <yutani.ini@gmail.com>
2018-07-07 01:01:18 +0900

    Handle other type of features

--- R/sf-utils.R
+++ R/sf-utils.R
@@ -3,6 +3,7 @@
 #' @name sf-utils
 #' @param x sf object.
 #' @param col_names Column names.
+#' @param fun Function to calcuate POINT from the geometry.
 #'
 #' @examples
 #' points_sfc <- sf::st_sfc(
@@ -15,19 +16,38 @@
 #'   geometry = points_sfc
 #' )
 #'
-#' st_add_point_coordinates(points_sf)
-#' st_add_point_coordinates(points_sf, col_names = c("long", "lat"))
+#' # For POINT, add the coordinates of the points.
+#' st_add_coordinates(points_sf)
+#' st_add_coordinates(points_sf, col_names = c("long", "lat"))
+#'
+#' # For other features, add the coordinates of the centroids of them.
+#' nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
+#' st_add_coordinates(nc)
+#'
+#' # You can specify other functions that return one POINT per geometry.
+#' st_add_coordinates(nc, fun = sf::st_point_ons_surface)
 NULL
 
 #' @rdname sf-utils
 #' @export
-st_add_point_coordinates <- function(x, col_names = NULL) {
-  if (!inherits(x, "sf")) stop("x is not a sf object!")
+st_add_coordinates <- function(x, col_names = NULL, fun = sf::st_centroid) {
+  if (!inherits(x, "sf")) stop("x is not a sf object!", call. = FALSE)
 
   x_sfc <- sf::st_geometry(x)
 
-  coordinates <- sf::st_coordinates(x_sfc)
+  if (inherits(x_sfc, "sfc_POINT")) {
+    coordinates <- sf::st_coordinates(x_sfc)
+  } else {
+    centroids <- fun(x_sfc)
+    coordinates <- sf::st_coordinates(centroids)
+  }
+
   if (!is.null(col_names)) {
+    len_args <- length(col_names)
+    ncol_coordinates <- ncol(coordinates)
+    if (len_args != ncol_coordinates) {
+      stop(glue::glue("col_names must be length {ncol_coordinates} (ncol of coordinates), not {len_args}"), call. = FALSE)
+    }
     colnames(coordinates)[seq_along(col_names)] <- col_names
   }
 
--- tests/testthat/test-sf.R
+++ tests/testthat/test-sf.R
@@ -10,11 +10,24 @@ points_sf <- sf::st_sf(
   geometry = points_sfc
 )
 
-test_that("st_add_point_coordinates() works", {
-  expect_equal(st_add_point_coordinates(points_sf),
+nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
+
+polygon_sf <- head(nc, 3)
+
+test_that("st_add_coordinates() works", {
+  expect_equal(st_add_coordinates(points_sf),
                sf::st_sf(value = c(10, 20), geometry = points_sfc,
                          X = 1:2, Y = 1:2))
-  expect_equal(st_add_point_coordinates(points_sf, col_names = c("x", "y")),
+  expect_equal(st_add_coordinates(points_sf, col_names = c("x", "y")),
                sf::st_sf(value = c(10, 20), geometry = points_sfc,
                          x = 1:2, y = 1:2))
+  # the length of col_names is wrong
+  expect_error(st_add_coordinates(points_sf, col_names = c("x")))
+
+  # sf_add_coordinates() accepts POINT
+  suppressWarnings(x <- st_add_coordinates(polygon_sf))
+  suppressWarnings(x2 <- sf::st_centroid(polygon_sf$geometry))
+  expect_equal(sf::st_point(c(x$X[1], x$Y[1])), x2[[1]])
+  expect_equal(sf::st_point(c(x$X[2], x$Y[2])), x2[[2]])
+  expect_equal(sf::st_point(c(x$X[3], x$Y[3])), x2[[3]])
 })
